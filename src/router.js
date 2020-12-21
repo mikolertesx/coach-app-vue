09fc34f62baa923d9940c9d5d869a7d926fcfa73
Mike <mgangsal@outlook.com>
2020-12-20 18:14:52 -0600

    Added AutoLogin

--- src/router.js
+++ src/router.js
@@ -44,7 +44,7 @@ const router = createRouter({
 router.beforeEach(function(to, from, next) {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     return next('/auth');
-  } else if (to.meta.requiresUnauth && !store.getters.isAuthenticated) {
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
     return next(from.fullPath);
   }
   return next();
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,36 +1,17 @@
 export default {
-  async login(_context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.error(responseData.error.message);
-      const error = new Error(
-        responseData.error.message || 'Failed to authenticate.'
-      );
-      throw error;
-    }
-
-    console.log(responseData);
-
-    _context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     });
   },
-  async signup(_context, payload) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_KEY}`;
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_KEY}`;
+
+    if (mode === 'signup') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_KEY}`;
+    }
 
     const response = await fetch(url, {
       method: 'POST',
@@ -40,7 +21,6 @@ export default {
         returnSecureToken: true
       })
     });
-
     const responseData = await response.json();
 
     if (!response.ok) {
@@ -51,13 +31,32 @@ export default {
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
 
-    _context.commit('setUser', {
+    context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    });
+  },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      });
+    }
   },
   logout(context) {
     context.commit('setUser', {
